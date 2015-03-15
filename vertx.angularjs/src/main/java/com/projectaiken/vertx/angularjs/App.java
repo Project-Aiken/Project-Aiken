bf6ae8abc8638f0a6dd17cfdcb4f3151f6458261
Tushar Nair <nairtu@gmail.com>
2015-03-15 18:31:39 -0400

    Initial Commit

---
+++ vertx.angularjs/src/main/java/com/projectaiken/vertx/angularjs/App.java
@@ -0,0 +1,13 @@
+package com.projectaiken.vertx.angularjs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
