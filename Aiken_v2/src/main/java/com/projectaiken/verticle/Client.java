8cf201ab5d096280ead669121790875cafbf3949
HackerHustler <elijah.haywood@gmail.com>
2015-05-11 20:42:41 -0500

    vertx server with frontend

--- Aiken_v2/src/main/java/com/projectaiken/verticle/Client.java
+++ Aiken_v2/src/main/java/com/projectaiken/verticle/Client.java
@@ -1,15 +1,15 @@
-package com.projectaiken.vertx.angularjs;
+package com.projectaiken.verticle;
 
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.platform.Verticle;
 
-public class Server extends Verticle {
+public class Client extends Verticle {
     public void start() {
         vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
             public void handle(HttpServerRequest req) {
                 String file = req.path().equals("/") ? "index.html" : req.path();
-                req.response().sendFile("webapp/" + file);
+                req.response().sendFile("../../../../" + file);
             }
         }).listen(8080);
     }
